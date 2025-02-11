dee335bd1c68337e0ed6b37c185ab9e495ddcdb9
Kendal Sosa <kendal.alfonso37@gmail.com>
2025-02-11 12:58:59 -0600

    chore: removiendo excepcion forzada en controlador.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,6 +3,5 @@ import logger from "../utils/logger";
 
 export const getUsers = (req: Request, res: Response) => {
   logger.info({ message: "Obteniendo usuarios", action: "getUsers" });
-  throw new Error("Algo ha salido mal");
   res.json({ users: ["Alice", "Bob", "Charlie"] });
 };
