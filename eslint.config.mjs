ff78ce9279e82e7adea20f6a55e0a3e9a1e2358d
Kendal Sosa <kendal.alfonso37@gmail.com>
2025-02-11 19:44:12 -0600

    Ajustes a ESLint y Prettier.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,12 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintConfigPrettier,
+  eslintConfigPrettier
 ];
